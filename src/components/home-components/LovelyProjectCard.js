a4dc0bfd2242a3809548a62d2018e26fdd38cf27
raikschneider <raik.schn@gmail.com>
2023-12-02 18:27:39 +0100

    added various design alternatives to project card

--- src/components/home-components/LovelyProjectCard.js
+++ src/components/home-components/LovelyProjectCard.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './lovely-project-card.css';
 import lovelyStrings from './lovelyProjectCardStrings.js';
-import screenshot from './../../images/a9bf1ec5b984e8348ae5cb5cb72f5cf1.jpg'
+import screenshot from './../../images/codelandinglm.png'
 
 export default function LovelyProjectCard({ language }) {
     return (
-        <div className="project-card-container">
-            <div className="project-card-description-container">
-                <h3>Lovely Manager</h3>
-                <span><p>{`${lovelyStrings[language.key].lovelyH3}`}</p><p>|</p><p>2023</p></span>
-                <p>{`${lovelyStrings[language.key].lovelyDescription}`}</p>
-                <button>{`${lovelyStrings[language.key].lovelyButton}`}</button>
-            </div>
-            <div className="project-card-image-container">
-                <img src={screenshot} alt="screenshot of project" />
-            </div>
+        //     <div className="project-card-container">
+        //         <div className="project-card-description-container">
+        //             <h3>Lovely Manager</h3>
+        //             <span><p>{`${lovelyStrings[language.key].lovelyH3}`}</p><p>|</p><p>2023</p></span>
+        //             {/* <p>{`${lovelyStrings[language.key].lovelyDescription}`}</p> */}
+        //             <button>{`${lovelyStrings[language.key].lovelyButton}`}</button>
+        //         </div>
+        //         <div className="project-card-image-container">
+        //             <img src={screenshot} alt="screenshot of project" />
+        //         </div>
+        //     </div>
+        // )
+        <div className="project-card-image-container">
+            <img src={screenshot} alt="screenshot of project" />
         </div>
+
     )
 }
---
+++ src/components/home-components/fhgfdh.js
@@ -0,0 +1,11 @@
+
+
+const LOVELY_MANAGER = {
+
+    type: "Cross-platform desktop application",
+
+    technologies: "Electron, React, Node.js",
+
+    description: "Design and development of a cross-plattform desktop application for a local frame manufacturer."
+
+}    
