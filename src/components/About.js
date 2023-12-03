6237d9141328e4edc1c8840c3cbba6245fd5c4d0
raikschneider <raik.schn@gmail.com>
2023-12-03 16:18:33 +0100

    added paragraphs and styling to about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import manaliso1 from './../images/manaliso1.jpg'
+import './about.css'
+import aboutStrings from './aboutStrings.js';
 
-export default function About({ setCurrentPage }) {
+export default function About({ language, setCurrentPage }) {
 
     return (
         <div className="about-container">
-            <img src={manaliso1} alt="man on a bike"></img>
+            <div className="about-images-container">
+                <img src={manaliso1} alt="man on a bike with surfboard"></img>
+            </div>
+            <div className="about-paragraphs-container">
+                <h1>{`${aboutStrings[language.key].aboutH1}`}</h1>
+                <p>{`${aboutStrings[language.key].aboutP1}`}</p>
+                <p>{`${aboutStrings[language.key].aboutP2}`}</p>
+                <p>{`${aboutStrings[language.key].aboutP3}`}</p>
+                <p>{`${aboutStrings[language.key].aboutP4}`}</p>
+            </div>
+
         </div>
     )
 }
---
+++ src/components/aboutStrings.js
@@ -0,0 +1,18 @@
+const aboutStrings = {
+    en: {
+        aboutH1: `Hey, me again`,
+        aboutP1: `I'm a former pro skater turned self-taught programmer based in Berlin. I'm 35 years of age and just became a father. I enjoy finding creative solutions to unique problems.`,
+        aboutP2: `I have taught myself the basics of programming and web development with the help of The Odin Project, books and Youtube. I progressed to small games, apps and websites to further hone my coding skills. I was always working on some project or another for the past three years since I started on this journey.`,
+        aboutP3: `While I was teaching myself how to program, I worked as a technical writer for Wix.com and taught people of all ages how to skateboard in my own little school here in Berlin. Breaking down a subject matter and teaching it to somebody in an understandable manner makes the analytical part of my brain jump with joy.`,
+        aboutP4: `When I'm not programming, I enjoy rock climbing, playing chess, reading all kinds of fiction and spending time with my family. I'm always on the lookout for something new I can sink my teeth into. Learning is the biggest joy in life and an integral part of my character.`,
+    },
+    de: {
+        aboutH1: `Hi, ich noch mal`,
+        aboutP1: `Ich bin ein ehemaliger Pro-Skater, der sich in den letzten 3 Jahren selbstständig das Programmieren beigebracht hat. Ich bin 35, aus Berlin und seit kurzem Vater. Das Finden kreativer Lösungen für außergewöhnliche Probleme macht mir Freude.`,
+        aboutP2: `Mithilfe des Odin Projekts, Büchern und Youtube habe ich mir die Grundlagen des Programmierens und der Webentwicklung angeeignet. Ich programmierte kleinere Spiele, Apps und später Webseiten für private Zwecke. Ich arbeite quasi immer an einem oder mehreren Projekten, um meine Fertigkeiten zu verbessern.`,
+        aboutP3: `Während ich mir das Programmieren beigebracht habe, habe ich als technischer Author für Wix.com gearbeitet und in meiner eigenen kleinen Schule Personen aus allen Altersgruppen das Skaten beigebracht. Eine Thematik in kleinere Teile aufzuspalten und verständlich zu vermitteln, bringt den analytischen Teil meines Hirns zum Tanzen.`,
+        aboutP4: `Wenn ich nichts für meine Entwicklerkarriere tue, verbringe ich meine Zeit in der Kletterhalle, beim Schachspielen, Lesen oder mit meiner Familie. Ich bin stets auf der Suche nach neuen interessanten Aktivitäten und Möglichkeiten, etwas dazuzulernen. Lernen ist der beste Zeitvertreib und ein wesentlicher Bestandteil meiner Persönlichkeit.`
+    }
+}
+
+export default aboutStrings
