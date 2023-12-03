e338cf0519084a41ff821d783fa4f264f4496292
raikschneider <raik.schn@gmail.com>
2023-12-03 13:55:14 +0100

    added icons and styling to technologies component

--- src/components/home-components/Technologies.js
+++ src/components/home-components/Technologies.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import './technologies.css';
+import jsIcon from './../../icons/icons8-javascript.svg';
+import expressIcon from './../../icons/icons8-express-js.svg';
+import gitIcon from './../../icons/icons8-git.svg';
+import reactIcon from './../../icons/icons8-react-native-68.png';
+import nodeIcon from './../../icons/icons8-nodejs-48.png';
+
 
 const technologyStrings = {
   en: {
-      technologyH2: `Technologies I'm familiar with`
+    technologyH2: `Some of the technologies I'm familiar with`
   },
   de: {
-      technologyH2: `Technologien, mit denen ich mich auskenne`
+    technologyH2: `Einige der Technologien, mit denen ich mich auskenne`
   }
 }
 
 export default function Technologies({ language }) {
   return (
     <div className="technologies-container">
-        <h2>{`${technologyStrings[language.key].technologyH2}`}</h2>
-        <div className="technologies-images-container"></div>
+      <h2>{`${technologyStrings[language.key].technologyH2}`}</h2>
+      <div className="technologies-images-container">
+        <div className="technologies-element-container">
+          <img src={jsIcon}></img><p>Javascript</p>
+        </div>
+        <div className="technologies-element-container">
+          <img src={reactIcon}></img><p>React</p>
+        </div>
+        <div className="technologies-element-container">
+          <img src={expressIcon}></img><p>ExpressJS</p>
+        </div>
+        <div className="technologies-element-container">
+          <img src={nodeIcon}></img><p>Node.js</p>
+        </div>
+        <div className="technologies-element-container">
+          <img src={gitIcon}></img><p>Git</p>
+        </div>
+      </div>
     </div>
   )
 }
