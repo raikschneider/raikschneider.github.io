2ae4a967a0af30955a0c414f130295fcc2111398
raikschneider <raik.schn@gmail.com>
2023-12-06 16:32:46 +0100

    improved phrasing

--- src/components/lovelyProjectStrings.js
+++ src/components/lovelyProjectStrings.js
@@ -1,6 +1,6 @@
 const lovelyProjectStrings = {
     en: {
-        lovelyP1: 'Lovely Manager is a cross-platform desktop application for Berlin frame manufacturing collective Lovely Projects. The app is estimated to save around 15 hours of work every week by automating cost and frame measurement calculations and outputting documents for clients and work sheets.'
+        lovelyP1: 'Lovely Manager is a cross-platform desktop application for Berlin frame manufacturing collective Lovely Projects. The app is estimated to save around 15 hours of work weekly by automating cost and frame measurement calculations and outputting documents for clients and work sheets.'
     },
     de: {
         lovelyP1: 'Lovely Manager ist eine plattformübergreifende Desktop-Anwendung für die Berliner Rahmenmanufaktur Lovely Projects. Die App ermöglicht ein geschätztes wöchentliches Zeitersparnis von 15 Arbeitsstunden durch automatisierte Kosten- und Rahmenmaßberechnungen, sowie das Ausgeben von Kundendokumenten und Laufzetteln für die Arbeiter.'
