665762e2b1b96b115fb88e678435f3bde24a1527
raikschneider <raik.schn@gmail.com>
2023-12-08 15:57:39 +0100

    added event listener on card container

--- src/components/home-components/ALSProjectCard.js
+++ src/components/home-components/ALSProjectCard.js
@@ -5,7 +5,7 @@ import ALS_EN from './../../images/carbonALS_EN.png';
 
 export default function ALSProjectCard({ language, setCurrentPage, toggleDropshadow }) {
   return (
-    <div className="project-card-image-container" onMouseEnter={() => toggleDropshadow(document.querySelector('#ALSimg'))} onMouseLeave={() => toggleDropshadow(document.querySelector('#ALSimg'))}>
+    <div className="project-card-image-container" onClick={() => setCurrentPage('ALSProject')} onMouseEnter={() => toggleDropshadow(document.querySelector('#ALSimg'))} onMouseLeave={() => toggleDropshadow(document.querySelector('#ALSimg'))}>
             <img src={language.key === 'de' ? ALS_DE : ALS_EN} alt="code snippet of project" id="ALSimg" />
             <button onClick={() => setCurrentPage('ALSProject')}>{`${buttonStrings[language.key].projectButton}`}</button>
         </div>
--- src/components/home-components/LovelyProjectCard.js
+++ src/components/home-components/LovelyProjectCard.js
@@ -6,7 +6,7 @@ import LM_EN from './../../images/carbonLM_EN.png';
 
 export default function LovelyProjectCard({ language, setCurrentPage, toggleDropshadow }) {
     return (
-        <div className="project-card-image-container" onMouseEnter={() => toggleDropshadow(document.querySelector('#LMimg'))} onMouseLeave={() => toggleDropshadow(document.querySelector('#LMimg'))}>
+        <div className="project-card-image-container" onClick={() => setCurrentPage('LovelyProject')} onMouseEnter={() => toggleDropshadow(document.querySelector('#LMimg'))} onMouseLeave={() => toggleDropshadow(document.querySelector('#LMimg'))}>
             <img src={language.key === 'de' ? LM_DE : LM_EN} alt="code snippet of project" id="LMimg" />
             <button onClick={() => setCurrentPage('LovelyProject')}>{`${buttonStrings[language.key].projectButton}`}</button>
         </div>
