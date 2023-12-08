00784ed9f293631d2360ce686c5be1225316357d
raikschneider <raik.schn@gmail.com>
2023-12-08 15:51:54 +0100

    added paragraphs to project, corrected phrasing

--- src/components/LovelyProject.js
+++ src/components/LovelyProject.js
@@ -7,6 +7,11 @@ export default function LovelyProject({ language }) {
     <div className="project-container">
       <h2>Lovely Manager</h2>
       <p>{`${lovelyProjectStrings[language.key].lovelyP1}`}</p>
+      <p>{`${lovelyProjectStrings[language.key].lovelyP2}`}</p>
+      <p>{`${lovelyProjectStrings[language.key].lovelyP3}`}</p>
+      <p>{`${lovelyProjectStrings[language.key].lovelyP4}`}</p>
+      <p>{`${lovelyProjectStrings[language.key].lovelyP5}`}</p>
+      <p>{`${lovelyProjectStrings[language.key].lovelyP6}`}</p>
     </div>
   )
 }
