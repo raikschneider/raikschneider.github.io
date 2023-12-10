69f87bdd79b154e3f57d648f2c764983be4ab2ef
raikschneider <raik.schn@gmail.com>
2023-12-10 15:22:32 +0100

    added box shadow and border radius to video

--- src/components/LovelyProject.js
+++ src/components/LovelyProject.js
@@ -18,8 +18,8 @@ export default function LovelyProject({ language }) {
       <div className="project-paragraph-container">
         <h3>{`${lovelyProjectStrings[language.key].lovelyH2}`}</h3>
         <p>{`${lovelyProjectStrings[language.key].lovelyP2}`}</p>
-        <div className="project-images">
-          <video width="750" height="500" autoPlay loop muted >
+        <div className="video-container">
+          <video autoPlay loop muted >
             <source src={Dropdown} type="video/mp4" />
           </video>
         </div>
