03149db09bbd53a960bed53e2daae3e18006940c
raikschneider <raik.schn@gmail.com>
2023-12-12 11:43:50 +0100

    added a few photos and made a collage

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import manaliso1 from './../images/manaliso1.jpg'
+import june1 from './../images/june1.jpg'
+import climbing1 from './../images/climbing1.jpg'
+import climbing2 from './../images/climbing2.jpg'
+import climbing3 from './../images/climbing3.jpg'
 import './about.css'
 import aboutStrings from './aboutStrings.js';
 
@@ -8,7 +12,9 @@ export default function About({ language, setCurrentPage }) {
     return (
         <div className="about-container">
             <div className="about-images-container">
-                <img src={manaliso1} alt="man on a bike with surfboard"></img>
+                <img src={june1} id="june" alt="hand of a baby"></img>
+                <img src={manaliso1} id="manaliso" alt="man on a bike with surfboard"></img>
+                <img src={climbing2} id="climbing" alt="climbing holds"></img>
             </div>
             <div className="about-paragraphs-container">
                 <h1>{`${aboutStrings[language.key].aboutH1}`}</h1>
