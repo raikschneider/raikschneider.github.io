d999f68479cab7860dc532f45b47091f78f73a9e
raikschneider <raik.schn@gmail.com>
2024-02-06 16:14:03 +0100

    added website link and click check behavior

--- src/components/home-components/ALSProjectCard.js
+++ src/components/home-components/ALSProjectCard.js
@@ -4,12 +4,19 @@ import ALS_DE from './../../images/carbonALS_DE.png';
 import ALS_EN from './../../images/carbonALS_EN.png';
 
 export default function ALSProjectCard({ language, switchPage, toggleDropshadow }) {
+
+  const alsURL = 'https://awhoulongboardschool.com'
+
+  const checkClick = (e) => {
+    if (e.target.tagName === 'BUTTON') return
+    switchPage('ALSProject')
+  }
   return (
-    <div className="project-card-image-container" onClick={() => switchPage('ALSProject')} onMouseEnter={() => toggleDropshadow(document.querySelector('#ALSimg'))} onMouseLeave={() => toggleDropshadow(document.querySelector('#ALSimg'))}>
+    <div className="project-card-image-container" onClick={(e) => checkClick(e)} onMouseEnter={() => toggleDropshadow(document.querySelector('#ALSimg'))} onMouseLeave={() => toggleDropshadow(document.querySelector('#ALSimg'))}>
       <img src={language.key === 'de' ? ALS_DE : ALS_EN} alt="code snippet of project" id="ALSimg" />
       <div className="project-card-button-container">
         <button onClick={() => switchPage('ALSProject')}>{`${buttonStrings[language.key].projectButton}`}</button>
-        <button onClick={() => window.open('https://awhoulongboardschool.com', '_blank')}>{`${buttonStrings[language.key].websiteButton}`}</button>
+        <button onClick={() => window.open(alsURL, '_blank')}>{`${buttonStrings[language.key].websiteButton}`}</button>
       </div>
     </div>
   )
