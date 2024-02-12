3b1b2eb5071446784a209e98c75d33ba52a8c193
raikschneider <raik.schn@gmail.com>
2024-02-12 16:49:44 +0100

    added hamburger menu and responsive behavior

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -27,14 +27,37 @@ export default function Nav({ language, setLanguage, switchPage }) {
             setLanguage({ key: 'de', string: 'EN' })
         }
     }
+
+    const toggleMenu = () => {
+        const hamburger = document.querySelector('.hamburger-menu')
+        const nav = document.querySelector('.nav-element-container')
+        hamburger.classList.toggle('active')
+        nav.classList.toggle('active')
+    }
+
+    const removeClass = () => {
+        const hamburger = document.querySelector('.hamburger-menu')
+        const nav = document.querySelector('.nav-element-container')
+        hamburger.classList.remove('active')
+        nav.classList.remove('active')
+    }
+
+
     return (
         <div className={`nav-container ${toggleSticky}`}>
-            <div className="page-link-element" onClick={() => switchPage('Home')}>{`${navStrings[language.key].homeDiv}`}</div>
-            <div className="page-link-element" onClick={() => switchPage('About')}>{`${navStrings[language.key].aboutDiv}`}</div>
-            <div
-                className="locale-toggle-container"
-                onClick={() => toggleLanguage(language)}>
-                <img src={globe} alt="globe icon" /><p>{`${language.string}`}</p>
+            <div className="nav-element-container">
+                <div className="page-link-element" onClick={() => { switchPage('Home'); removeClass() }}>{`${navStrings[language.key].homeDiv}`}</div>
+                <div className="page-link-element" onClick={() => { switchPage('About'); removeClass() }}>{`${navStrings[language.key].aboutDiv}`}</div>
+                <div
+                    className="locale-toggle-container"
+                    onClick={() => toggleLanguage(language)}>
+                    <img src={globe} alt="globe icon" /><p>{`${language.string}`}</p>
+                </div>
+            </div>
+            <div className="hamburger-menu" onClick={() => toggleMenu()}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
             </div>
         </div>
     )
