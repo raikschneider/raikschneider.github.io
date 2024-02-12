9c03c295d0d5bd8c9002be435fa0c13f085dd0e5
raikschneider <raik.schn@gmail.com>
2024-02-12 14:20:20 +0100

    added summary and premise, modified text

--- src/components/ALSProject.js
+++ src/components/ALSProject.js
@@ -3,6 +3,7 @@ import alsProjectStrings from './alsProjectStrings.js';
 import Locale from './../images/Locale_Video.mp4';
 import ALSCover from './../images/ALS_Profile.png';
 import ALSMovie from './../images/ALS_movie.mp4';
+import ALSPremise from './../images/ALS_Premise.jpg'
 
 export default function ALSProject({ language }) {
   return (
@@ -22,6 +23,13 @@ export default function ALSProject({ language }) {
             <h1>TECHNOLOGIES USED</h1>React | CSS</div>
         </div>
       </div>
+      <div className="project-premise-wrapper">
+        <div className="project-premise-container">
+          <h3>{`${alsProjectStrings[language.key].alsPremiseH3}`}</h3>
+          <p>{`${alsProjectStrings[language.key].alsPremise}`}</p>
+        </div>
+        <img src={ALSPremise} alt={'skateboards illustration'} />
+      </div>
       <div className="project-paragraph-container">
         <h3>{`${alsProjectStrings[language.key].alsH2}`}</h3>
         <p>{`${alsProjectStrings[language.key].alsP2}`}</p>
@@ -38,6 +46,13 @@ export default function ALSProject({ language }) {
           <video autoPlay loop muted >
             <source src={Locale} type="video/mp4" />
           </video>
+        </div>
+      </div>
+      <div className="project-summary-wrapper">
+        <img src={ALSPremise} />
+        <div className="project-summary-container">
+          <h3>{`${alsProjectStrings[language.key].alsSummaryH3}`}</h3>
+          <p>{`${alsProjectStrings[language.key].alsSummary}`}</p>
         </div>
       </div>
     </div>
