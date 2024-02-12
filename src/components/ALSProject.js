368e302b059439c07cd46dabb652297d160343a4
raikschneider <raik.schn@gmail.com>
2024-02-12 13:38:06 +0100

    added movie and alt text

--- src/components/ALSProject.js
+++ src/components/ALSProject.js
@@ -2,12 +2,13 @@ import React  from 'react';
 import alsProjectStrings from './alsProjectStrings.js';
 import Locale from './../images/Locale_Video.mp4';
 import ALSCover from './../images/ALS_Profile.png';
+import ALSMovie from './../images/ALS_movie.mp4';
 
 export default function ALSProject({ language }) {
   return (
     <div className="project-container">
       <div className="project-cover-container">
-        <img src={ALSCover} alt='rack displaying finished frames' />
+        <img src={ALSCover} alt='AWHOU! Longboard School logo' />
         <div className="project-details-container">
           <div className="project-details-header-container">
             <h1>AWHOU! LONGBOARD SCHOOL</h1>
@@ -24,7 +25,10 @@ export default function ALSProject({ language }) {
       <div className="project-paragraph-container">
         <h3>{`${alsProjectStrings[language.key].alsH2}`}</h3>
         <p>{`${alsProjectStrings[language.key].alsP2}`}</p>
-        <div className="project-images">
+        <div className="video-container">
+        <video autoPlay loop muted >
+            <source src={ALSMovie} type="video/mp4" />
+          </video>
         </div>
       </div>
       <div className="project-paragraph-container">
